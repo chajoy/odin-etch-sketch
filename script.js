c45d39cbd5f34a8b54777f47cd065fc96f65fb3e
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-04 13:37:27 +0000

    Add initial files and functionality
    Add index.html, script.js and style.css
    Create function to generate 16x16 grid of div's

---
+++ script.js
@@ -0,0 +1,19 @@
+const grid_container = document.querySelector(`#grid-container`);
+
+const createTable = function ()
+{
+    for(x = 0; x < 16; x++)
+    {
+        console.log(`row`);
+        const row = document.createElement(`div`);
+        row.classList.add(`row`);
+        for(y = 0; y < 16; y++)
+        {
+            const box = document.createElement(`div`);
+            box.classList.add(`box`);
+            row.appendChild(box);
+        }
+        grid_container.appendChild(row);
+    }
+
+}
