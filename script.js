fbdc83d1c411d5b38ec0652b7dc1aa076f5d819c
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-04 15:08:07 +0000

    Add mousedown functionality

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const grid_container = document.querySelector(`#grid-container`);
 const btn_create_grid = document.querySelector(`#btn-create-grid`);
 
-const createTable = function ()
-{
-    for(x = 0; x < 16; x++)
-    {
+let mousedown = false;
+
+document.addEventListener(`mouseup`, () => mousedown = false);
+document.addEventListener(`mousedown`, () => mousedown = true);
+
+const createTable = function () {
+    for (let x = 0; x < 16; x++) {
         const column = document.createElement(`div`);
         column.classList.add(`column`);
-        for(y = 0; y < 16; y++)
-        {
+        for (let y = 0; y < 16; y++) {
             const box = document.createElement(`div`);
             box.classList.add(`box`);
-            box.addEventListener(`mouseenter`, () =>
-            {
-                box.classList.add(`active`);
+
+            box.addEventListener(`mousedown`, () => mousedown = true);
+
+            box.addEventListener(`mouseover`, () => {
+                if (mousedown) {
+                    box.classList.add(`active`);
+                }
             })
+
             column.appendChild(box);
         }
         grid_container.appendChild(column);
@@ -22,8 +29,7 @@ const createTable = function ()
 
 }
 
-btn_create_grid.addEventListener(`click`, () =>
-{
+btn_create_grid.addEventListener(`click`, () => {
     createTable();
     btn_create_grid.remove();
 })
