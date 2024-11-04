3a6c85184427c028e858da58c7bfa2a4b42d4c23
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-04 15:39:19 +0000

    Dynamic box sizes

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const grid_container = document.querySelector(`#grid-container`);
 const btn_create_grid = document.querySelector(`#btn-create-grid`);
+const GRID_SIZE = 800;
 
 let mousedown = false;
 
 document.addEventListener(`mouseup`, () => mousedown = false);
 document.addEventListener(`mousedown`, () => mousedown = true);
 
-const createTable = function () {
-    for (let x = 0; x < 16; x++) {
+const createTable = function (amount = 16) {
+    let cellSize = 800 / amount;
+    for (let x = 0; x < amount; x++) {
         const column = document.createElement(`div`);
         column.classList.add(`column`);
-        for (let y = 0; y < 16; y++) {
+        for (let y = 0; y < amount; y++) {
             const box = document.createElement(`div`);
+            box.style.height = cellSize + `px`;
+            box.style.width = cellSize + `px`;
+
             box.classList.add(`box`);
 
             box.addEventListener(`mousedown`, () => mousedown = true);
@@ -31,5 +36,4 @@ const createTable = function () {
 
 btn_create_grid.addEventListener(`click`, () => {
     createTable();
-    btn_create_grid.remove();
 })
